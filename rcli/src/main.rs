66388b1be5e188a34b9e79b17e380c314c87ef86
chuangmao11 <602700987@qq.com>
2025-05-15 14:49:22 +0800

    password strength

--- rcli/src/main.rs
+++ rcli/src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
-use rcli::{Opts,SubCommand,process_csv};
+use rcli::{Opts,SubCommand,process_csv,process_genpass};
 //cargo run -- csv -i /Users/alex/Desktop/r-project/rcli/assets/juventus.csv -o output.json
+//cargo run -- genpass -l 16 >output.txt
+//cargo run -- csv -i /Users/alex/Desktop/r-project/rcli/assets/juventus.csv --format yaml
 fn main() ->anyhow::Result<()>{
     let opts =Opts::parse();
     match opts.cmd{
@@ -11,6 +13,15 @@ fn main() ->anyhow::Result<()>{
                 format!("output.{}",opts.format)
             };
             process_csv(&opts.input,output,opts.format)?;
+        }
+        SubCommand::Genpass(opts) =>{
+            let password = process_genpass(
+                opts.length,
+                opts.uppercase,
+                opts.lowercase,
+                opts.number,
+                opts.symbol)?;
+            println!("{}", password);
         }
     }
     Ok(())
--- rcli/src/opts.rs
+++ rcli/src/opts.rs
@@ -12,7 +12,10 @@ pub struct Opts{
 #[derive(Parser,Debug)]
 pub enum SubCommand{
     #[command(name="csv",about="show cssv,or convert to csv to other formats")]
-    Csv(CsvOpts)
+    Csv(CsvOpts),
+
+    #[command(name="genpass",about="generate a random password")]
+    Genpass(GenPassOpts)
 }
 
 #[derive(Debug,Clone,Copy)]
@@ -36,6 +39,24 @@ pub struct CsvOpts{
 
     #[arg(long,default_value_t=true)]
     pub header:bool,
+}
+
+#[derive(Debug,Parser)]
+pub struct GenPassOpts{
+    #[arg(short,long,default_value_t=16)]
+    pub length:u8,
+
+    #[arg(long,default_value_t=true)]
+    pub uppercase:bool,
+
+    #[arg(long,default_value_t=true)]
+    pub lowercase:bool,
+
+    #[arg(long,default_value_t=true)]
+    pub number:bool,
+
+    #[arg(long,default_value_t=true)]
+    pub symbol:bool,
 }
 
 fn verify_input_file(filename:&str) ->Result<String,&'static str>{
